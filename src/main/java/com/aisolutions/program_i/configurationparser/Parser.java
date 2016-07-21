2ea81722a4cb55d57714955ade0b8ae2b339a910
zakrywilson <zakrywilson@gmail.com>
2016-07-21 00:48:55 -0600

    Added a parser to parse config file

---
+++ src/main/java/com/aisolutions/program_i/configurationparser/Parser.java
@@ -0,0 +1,42 @@
+package com.aisolutions.program_i.configurationparser;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Set;
+
+public class Parser {
+
+    private File file;
+
+    public Parser(final String configFilePath) throws IllegalArgumentException, FileNotFoundException {
+        initialize(configFilePath);
+    }
+
+    private void initialize(final String path) throws FileNotFoundException {
+        if (path == null || path.trim().length() == 0) {
+            throw new IllegalArgumentException("Invalid config file path: " + path);
+        }
+        file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException("File does not exist: " + path);
+        }
+        if (!file.isFile()) {
+            throw new FileNotFoundException("File is not a file. Cannot process file: " +
+                                            file.getAbsolutePath());
+        }
+    }
+
+    public Set parseConfigFile() throws IOException {
+        List<String> lines = FileUtils.readLines(file, StandardCharsets.US_ASCII);
+        for (String line : lines) {
+            // TODO implement
+        }
+        return null;
+    }
+
+}
