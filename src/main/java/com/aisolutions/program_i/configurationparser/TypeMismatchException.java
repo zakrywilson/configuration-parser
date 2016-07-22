8ff38d3ca86f35a45f44d0f36694614408789e84
zakrywilson <zakrywilson@gmail.com>
2016-07-21 23:14:58 -0600

    Adding Javadocs to TypeMismatchException

--- src/main/java/com/aisolutions/program_i/configurationparser/TypeMismatchException.java
+++ src/main/java/com/aisolutions/program_i/configurationparser/TypeMismatchException.java
@@ -1,21 +1,61 @@
 package com.aisolutions.program_i.configurationparser;
 
-public class TypeMismatchException extends RuntimeException {
+/**
+ * Represents an inability to parse a <tt>String</tt> due to a type mismatch.
+ *
+ * @author Zach Wilson
+ */
+public class TypeMismatchException extends Exception {
 
+    /**
+     * Constructs a new runtime exception with null as its detail message. The cause is not
+     * initialized, and may subsequently be initialized by a call to
+     * {@link Throwable#initCause(java.lang.Throwable)}.
+     */
     public TypeMismatchException() {
         super();
     }
 
+    /**
+     * Constructs a new runtime exception with the specified detail message. The cause is not
+     * initialized, and may subsequently be initialized by a call to
+     * {@link Throwable#initCause(java.lang.Throwable)}.
+     *
+     * @param msg the detail message. The detail message is saved for later retrieval by the
+     *            {@link Throwable#getMessage()} method.
+     */
     public TypeMismatchException(final String msg) {
         super(msg);
     }
 
-    public TypeMismatchException(final Throwable t) {
-        super(t);
+    /**
+     * Constructs a new runtime exception with the specified cause and a detail message of
+     * <tt>(cause==null ? null : cause.toString())</tt> (which typically contains the class and
+     * detail message of cause). This constructor is useful for runtime exceptions that are
+     * little more than wrappers for other throwables.
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link Throwable#getCause()} method). (A <tt>null</tt> value is permitted, and
+     *              indicates that the cause is nonexistent or unknown.)
+     */
+    public TypeMismatchException(final Throwable cause) {
+        super(cause);
     }
 
-    public TypeMismatchException(final String msg, final Throwable t) {
-        super(msg, t);
+    /**
+     * Constructs a new runtime exception with the specified detail message and cause.
+     * <p>
+     * Note that the detail message associated with cause is not automatically incorporated in this
+     * runtime exception's detail message.
+     *
+     * @param msg the detail message (which is saved for later retrieval by the
+     *            {@link Throwable#getMessage()} method).
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link Throwable#getCause()} method). (A <tt>null</tt> value is permitted, and
+     *              indicates that the cause is nonexistent or unknown.)
+     */
+    public TypeMismatchException(final String msg, final Throwable cause) {
+        super(msg, cause);
     }
 
 }
